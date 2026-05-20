f988f262a1f7d2c8330d28e708fd03b74db65c61
beatrizcampos25 <beatrizcalves4015@gmail.com>
2026-05-20 16:49:46 -0300

    commit

--- aula20/src/model/Produto.ts
+++ aula20/src/model/Produto.ts
@@ -1,19 +1,25 @@
 import { Fabricante } from "./Fabricante"
-	
+
 export class Produto {
+
     id: number
     nome: string
     preco: number
     fabricante: Fabricante
-    
-    constructor(nome: string, preco: number, fabricante: Fabricante) {
+
+    constructor(
+        nome: string,
+        preco: number,
+        fabricante: Fabricante
+    ) {
+
+        this.id = this.geraId()
         this.nome = nome
         this.preco = preco
         this.fabricante = fabricante
-        this.id = this.geraId () ;
     }
 
-    private geraId () : number {
-        return Date . now () ;
+    private geraId(): number {
+        return Date.now()
     }
 }
